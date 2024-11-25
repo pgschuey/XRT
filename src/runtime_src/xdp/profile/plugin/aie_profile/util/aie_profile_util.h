81f0cd921ad0cfc54654da3f33104b8eee68374d
Paul Schumacher <schuey@xilinx.com>
2024-11-25 10:35:05 -0700

    more bug fixes
    Signed-off-by: Paul Schumacher <schuey@xilinx.com>

--- src/runtime_src/xdp/profile/plugin/aie_profile/util/aie_profile_util.h
+++ src/runtime_src/xdp/profile/plugin/aie_profile/util/aie_profile_util.h
@@ -100,13 +100,6 @@ namespace xdp::aie::profile {
   std::map<std::string, std::vector<uint32_t>> getMicrocontrollerEventSets(const int hwGen);
 
   /**
-   * @brief   Get metric sets for debug modules in microcontrollers
-   * @return  Map of microcontroller metric set names with vectors of event IDs
-   */
-  //std::map<std::string, std::vector<XAie_Events>> getMicrocontrollerEventSets();
-  std::map<std::string, std::vector<uint32_t>> getMicrocontrollerEventSets();
-
-  /**
    * @brief  Modify configured events
    * @param type module type 
    * @param subtype plio/gmio type
